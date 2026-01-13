e63fea550eac9e217969bd1a2abb55f72355a39f
mudabirkowsar <mudabirkhanday@gmail.com>
2026-01-13 17:57:33 +0530

    updated database for this project

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/business");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Database connected");
     } catch (error) {
         console.error("Error connecting to database:", error.message);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors");
-const connectDB = require("./models/db");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
